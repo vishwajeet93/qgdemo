d2addef7aea8312b6c68c40c940f50ab7680e386
saikalyan9981 <30959693+saikalyan9981@users.noreply.github.com>
2018-01-04 18:04:08 +0530

    Add files via upload

--- chrome-plugin/mypopup.js
+++ chrome-plugin/mypopup.js
@@ -1,49 +1,62 @@
 function onPageDetailsReceived(pageDetails)  {
-    document.getElementById('title').value = pageDetails.title;
-    document.getElementById('url').value = pageDetails.url;
     document.getElementById('summary').innerText = pageDetails.summary;
 }
 var statusDisplay = null;
 function addBookmark() {
-    event.preventDefault();
-    var postUrl = 'http://post-test.local.com';
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', postUrl, true);
-    var title = encodeURIComponent(document.getElementById('title').value);
-    var url = encodeURIComponent(document.getElementById('url').value);
-    var summary = encodeURIComponent(document.getElementById('summary').value);
-    var tags = encodeURIComponent(document.getElementById('tags').value);
-    var params = 'title=' + title +
-                 '&url=' + url +
-                 '&summary=' + summary +
-                 '&tags=' + tags;
-
-    params = params.replace(/%20/g, '+');
-
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-    xhr.onreadystatechange = function() {
-        // If the request completed
-        if (xhr.readyState == 4) {
-            statusDisplay.innerHTML = '';
-            if (xhr.status == 200) {
-                // If it was a success, close the popup after a short delay
-                statusDisplay.innerHTML = 'Saved!';
-                window.setTimeout(window.close, 1000);
-            } else {
-                // Show what went wrong
-                statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
-            }
+  event.preventDefault();
+  var summary = document.getElementById('summary').value;
+  var url = 'http://localhost:9000/?properties=%7B%22annotators%22:%22tokenize,ssplit%22,%22outputFormat%22:%22json%22%7D';
+  console.log(summary);
+  jQuery.ajax({
+  url : url,
+  type: "POST",
+  data : summary,
+  success : function(res){
+      console.log(res);
+      var sentsarr = res["sentences"];
+      var sents = [];
+      var sent = '';
+      for (var i=0;i<sentsarr.length;i++){
+        for (var j=0;j<sentsarr[i]["tokens"].length;j++){
+          sent = sent + sentsarr[i]["tokens"][j]["word"] + ' ';
         }
-    };
+        sents.push(sent);
+        sent = '';
+      }
+      console.log(sents);
+      console.log(sents[0]);
+      var data = [{"src" :null,"beam_size":5,"replace_unk":"true",
+          "withAttn":"true"}];
+      var i,dat;
+      qna.innerHTML = '';
+      for (i in sents){
+          data[0].src = sents[i];
+          dat = JSON.stringify(data);
+          console.log(data);
+          var targetUrl = 'http://52.172.194.2:7784/translator/translate';
+          var proxyUrl ='https://cors-anywhere.herokuapp.com/';
+          var postUrl = proxyUrl+targetUrl;
+        jQuery.ajax({
+            type: "POST",
+            data : dat,
+            url : postUrl,
+            dataType:'json',
+            contentType: "application/json",
+            success: function(res) {
+                console.log(res);
+              qna.innerHTML +=  "<li class= list-group-item>" + res[0][0]["tgt"] + "</li>";
+              qna.innerHTML +=  "<li class = 'list-group-item list-group-item-success' >" + res[0][0]["src"] + "</li>" + "<br/>";
+            }
+        });
+      }
+    }
+    });
 
-    // Send the request and set status
-    xhr.send(params);
-    statusDisplay.innerHTML = 'Saving...';
 }
 
 window.addEventListener('load', function(evt) {
     // Cache a reference to the status display SPAN
+    qna = document.getElementById('qna');
     statusDisplay = document.getElementById('status-display');
     // Handle the bookmark form submit event with our addBookmark function
     document.getElementById('addbookmark').addEventListener('submit', addBookmark);
